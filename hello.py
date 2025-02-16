880382e337269dd2a589b1ffc9660cb5a9a84161
Maria Gorelik <marya.gorelik@gmail.com>
2025-02-16 13:54:01 +0300

    Add docker-compose

--- hello.py
+++ hello.py
@@ -17,6 +17,8 @@ async def ping():
 @app.post("/predict")
 async def predict(request: PredictRequest):
     try:
+        if request.text[-1] != '.':
+            request.text += '.'
         input_ids = tokenizer(request.text, return_tensors="pt").input_ids
         outputs = model.generate(input_ids)
         translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
@@ -26,4 +28,4 @@ async def predict(request: PredictRequest):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=800)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
