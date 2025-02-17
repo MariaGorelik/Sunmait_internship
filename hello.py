42bc740684ab96d83e0cb54545c579250e1e71c2
Maria Gorelik <marya.gorelik@gmail.com>
2025-02-17 22:42:29 +0300

    modigy hello.py

--- hello.py
+++ hello.py
@@ -1,24 +1,28 @@
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from transformers import T5Tokenizer, T5ForConditionalGeneration
+import uvicorn
 
 app = FastAPI()
 
 tokenizer = T5Tokenizer.from_pretrained("t5-small")
 model = T5ForConditionalGeneration.from_pretrained("t5-small")
 
+
 class PredictRequest(BaseModel):
     text: str
+
 
 @app.get("/ping")
 async def ping():
     return {"message": "server is working"}
 
+
 @app.post("/predict")
 async def predict(request: PredictRequest):
     try:
-        if request.text[-1] != '.':
-            request.text += '.'
+        if request.text[-1] != ".":
+            request.text += "."
         input_ids = tokenizer(request.text, return_tensors="pt").input_ids
         outputs = model.generate(input_ids)
         translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
@@ -26,6 +30,6 @@ async def predict(request: PredictRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
+
 if __name__ == "__main__":
-    import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
